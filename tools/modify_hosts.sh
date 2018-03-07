c440714aba2ba533c3689fee1a73f708474c61ca
zhouyq <zhouyq@goodrain.com>
2018-03-07 14:05:36 +0800

    增加http-server

--- tools/modify_hosts.sh
+++ tools/modify_hosts.sh
@@ -5,7 +5,8 @@ CLUSTER_RULE=$1
 HOST_FILE="/etc/hosts"
 
 function manage_opt(){
-  HOST_LIST="hub.goodrain.com goodrain.me"
+  HOST_LIST="goodrain.me"
+  HTTP_SERVER="repo.goodrain.com dl.repo.goodrain.com"
 
   for host in $HOST_LIST
   do
@@ -16,17 +17,35 @@ function manage_opt(){
       echo -e "\e[31m$host is already set.\e[0m"
     fi
   done
+
+  for host in $HTTP_SERVER
+  do
+    isHostSet=`grep $host /etc/hosts`
+    if [ "$isHostSet" == "" ];then
+      echo "172.30.42.100 $host" >> $HOST_FILE
+    else
+      echo -e "\e[31m$host is already set.\e[0m"
+    fi
+  done
 }
 
 function compute_opt(){
   echo
   manage_ip=$1
-  isHostSet=`grep hub.goodrain.com /etc/hosts`
-  if [ "$isHostSet" == "" ];then
-     echo -e "${manage_ip}\thub.goodrain.com" >> $HOST_FILE
-  else
-     echo -e "\e[32mhub.goodrain.com\e[0m is already set."
-  fi
+  HTTP_SERVER="repo.goodrain.com dl.repo.goodrain.com"
+
+  for host in $HTTP_SERVER
+  do
+    isHostSet=`grep $host /etc/hosts`
+    if [ "$isHostSet" == "" ];then
+      echo "172.30.42.100 $host" >> $HOST_FILE
+    else
+      echo -e "\e[31m$host is already set.\e[0m"
+    fi
+  done
+
+
+
 }
 
 #========= main =========
--- tools/setup_http_server.sh
+++ tools/setup_http_server.sh
@@ -8,7 +8,10 @@ if [ "$isOK" == "Y" -o "$isOK" == "y" ];then
 
   # set virtual ip
   ifconfig docker0:1 172.30.42.100 netmask 255.255.255.0 up
-  
+
+  # clear container if exist
+  docker stop http_server
+  docker rm http_server
   # copy ssl key
   [ ! -d /etc/goodrain/http_server/ssl/ ] && mkdir -pv /etc/goodrain/http_server/ssl/
   cp -rp $PWD/modules/http_server/ssl/goodrain.com /etc/goodrain/http_server/ssl/
@@ -21,14 +24,10 @@ if [ "$isOK" == "Y" -o "$isOK" == "y" ];then
   # make build dir
   [ ! -d /grdata/build/lang-env ] && mkdir -pv /grdata/build/lang-env
 #  cat $PWD/rbdimg/rbd-registry_2.3.1.gz | docker load \
-  docker run -d -v $PWD:/mnt \
+  docker run -d --name http_server -v $PWD:/mnt \
   -v /etc/goodrain/http_server/sites:/usr/local/tengine/conf/sites \
   -v /etc/goodrain/http_server/ssl:/usr/local/tengine/conf/ssl \
   --network host rainbond/rbd-proxy:3.4
 else
   echo "Skip setup http server."
 fi
-
-
-      - 
-      - 
