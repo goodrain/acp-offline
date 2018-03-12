35f021087890c46a055aeb94b42435e4127df29a
zhengh <zhengh@goodrain.com>
2018-03-12 15:22:29 +0800

    [FIX] 修复离线版域名问题

--- tools/post_install.sh
+++ tools/post_install.sh
@@ -6,7 +6,7 @@ cp -rp $PWD/grdata/build/lang-env/* /grdata/build/lang-env/ && echo OK
 #cp -rp $PWD/app_publish  /grdata/build/tenant/ && echo OK 
 
 # import sql data
-echo "\e[32mimport sql data...\e[0m"
+echo "import sql data..."
 $PWD/modules/rbd_db/sql/import_sql.sh
 
 # copy acp_proxy
@@ -18,3 +18,20 @@ echo -ne "\e[32mStoping temporary service...\e[0m"
 /usr/local/nginx/sbin/nginx -s stop
 echo -ne "\e[32mRemoving temporary files...\e[0m"
 rm -rf /usr/local/nginx
+
+# add proxy server key
+echo -ne "add proxy server key"
+\cp -rp goodrain.com  /etc/goodrain/proxy/ssl/
+
+# config proxy
+echo -ne "config proxy"
+\cp -rp $PWD/tools/registry /etc/goodrain/proxy/sites/registry
+
+# adapt new config
+echo -ne "config proxy"
+dc-compose stop
+cclear
+systemctl restart docker
+dc-compose up -d
+
+\cp $PWD/tools/chaos_config.json /etc/goodrain/etc/chaos/config.json 
